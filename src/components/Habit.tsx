80099bed55e3c92196782da80282137cc3f85307
Dionnatan Alves <dionn_a.p@hotmail.com>
2023-01-19 18:32:23 -0300

    atualização: mudança do nome do arquivo

--- src/components/Habit.tsx
+++
@@ -1,9 +0,0 @@
-interface HabitProps {
-  completed: number
-}
-
-export function HabitDay() {
-  return (
-    <div className="w-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg cursor-pointer"></div>
-  );
-}
